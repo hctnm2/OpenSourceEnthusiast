36f94f4f4034ba0be8532f10f6ebad9819f1bb03
harry91011 <115446299+harry91011@users.noreply.github.com>
2022-10-17 08:42:48 +0545

    Create variable.php

---
+++ codes/variable.php
@@ -0,0 +1,45 @@
+<?php
+
+/*contoh*/
+$data = 7;
+
+$nilai = $data;
+
+echo "$nilai<br>";
+
+/*contoh 2*/
+$nama = "Richard gans";
+$siswa = $nama;
+
+echo "$siswa <br>";
+
+/*contoh 3*/
+$nama = "richard gans";
+$nilai = 90;
+
+echo "Selamat $nama Kamu mendapatkan nilai $nilai <br>";
+
+
+/*contoh xixixid*/
+$minum = 'Teh';
+$makanan = 'Mie';
+
+echo "Pagi-Pagi Enak Nya Minum $minum nih <br>";
+echo "Hujan-hujan gini enak-nya makan $makanan nih";
+
+echo "<hr>";
+
+
+ $a = "Nama";
+ $b = 123;
+ $c = array();
+ $d = Null;
+ $e = 0.4;
+ $f = true;
+ var_dump($a)."<br>";
+ var_dump($b)."<br>";
+ var_dump($c)."<br>";
+ var_dump($d)."<br>";
+ var_dump($e)."<br>";
+ var_dump($f)."<br>";
+?>
